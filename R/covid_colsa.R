9658a538c597caac550a17aeced6272efbe983d3
rahul-bharadwaj <rmys0001@student.monash.edu>
2020-10-19 22:54:57 +1100

    Documenting the 3 data sets

---
+++ R/covid_colsa.R
@@ -0,0 +1,8 @@
+#' Covid-19 Data for Colombia & South Africa
+#' 
+#' This data contains 35 variables for Covid stats in Colombia and South Africa from January 2020.
+#' 
+#' @source
+#' The data comes from Guidotti, E., Ardia, D., (2020), "COVID-19 Data Hub", Journal of Open Source Software 5(51):2376, doi: 10.21105/joss.02376, through the R package "COVID19" and filtered for the country Colombia and South Africa.
+#'
+"covid_colsa"
