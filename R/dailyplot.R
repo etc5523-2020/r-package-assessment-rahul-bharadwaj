1f3c2aa900470e17cf43480be4d9d8e93efc473b
rahul-bharadwaj <rmys0001@student.monash.edu>
2020-10-20 18:21:29 +1100

    Adding dailyplot() and its documentation

---
+++ R/dailyplot.R
@@ -0,0 +1,16 @@
+#' Daily Count Plot
+#' 
+#' This function automatically launches the Shiny App and makes it available to the user to have an overview of how the datasets are used to compare the stats in Colombia and South Africa. This App can be used as a starting point to learn the potential visualizations using the data available in 'covidcolsa'.
+#' 
+#' @param data A dataset which consists of daily counts of all attributes.
+#' @export
+dailyplot <- function(data){
+  data %>% ggplot() +
+    geom_line(aes(x = date, y = dailytest, color = "dailytested")) +
+    geom_line(aes(x = date, y = dailyconfirm, color = "dailyconfirmed")) +
+    geom_line(aes(x = date, y = dailyrecover, color = "dailyrecovered")) +
+    geom_line(aes(x = date, y = dailydeaths, color = "dailydeaths")) +
+    xlab("Date") + ylab("Attribute Counts") +
+    ggtitle("Daily Timeline Curves for Covid") +
+    theme_light()
+}
